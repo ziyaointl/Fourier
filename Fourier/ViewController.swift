1aefa16662f26424a80480f8a6fdea03750a8cdb
Ziyao Zhang <ziyaointl@gmail.com>
2018-03-15 15:10:31 +0800

    Initial Commit

---
+++ Fourier/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fourier
+//
+//  Created by Blocry Glass on 3/15/18.
+//  Copyright © 2018 Blocry Glass. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
