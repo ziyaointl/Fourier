948c446902e1fd322b25cc135835cc6fd40baf16
Ziyao Zhang <ziyaointl@gmail.com>
2018-03-16 10:26:58 +0800

    Add AudioManagerDelegate

---
+++ Fourier/AudioManagerDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AudioManagerDelegate.swift
+//  Fourier
+//
+//  Created by Blocry Glass on 3/16/18.
+//  Copyright © 2018 Blocry Glass. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+protocol AudioManagerDelegate: NSObjectProtocol {
+    func didFourierTransform(_ audioManager: AudioManager, output: Array<Float>)
+}
