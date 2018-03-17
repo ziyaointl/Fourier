e2d24197856742cef743e6e28a02a549138581c8
Ziyao Zhang <ziyaointl@gmail.com>
2018-03-17 23:39:21 +0800

    Fix indentation

--- Fourier/AudioManager.swift
+++ Fourier/AudioManager.swift
@@ -41,36 +41,36 @@ class AudioManager {
     }
     
     func fourierTransform(buffer: AVAudioPCMBuffer) {
-            let frameCount = buffer.frameLength
-            let frameCountInLog2 = vDSP_Length(log2(Double(frameCount)))
-            if let fftSetup = vDSP_create_fftsetup(frameCountInLog2, FFTRadix(FFT_RADIX2)) {
-                var realp = [Float](repeating: 0.0, count: Int(frameCount / 2))
-                var imagp = [Float](repeating: 0.0, count: Int(frameCount / 2))
-                var output = DSPSplitComplex(realp: &realp, imagp: &imagp)
-                
-                // Apply Window Function
-                var window = [Float](repeating: 0, count: Int(frameCount))
-                vDSP_hann_window(&window, vDSP_Length(frameCount), Int32(vDSP_HANN_NORM))
-                vDSP_vmul((buffer.floatChannelData?.pointee)!, 1, window, 1, (buffer.floatChannelData?.pointee)!, 1, vDSP_Length(frameCount))
-                
-                buffer.floatChannelData?.pointee.withMemoryRebound(to: DSPComplex.self, capacity: Int(frameCount)) { bufferAsComplexArray in
-                    let bufferAsComplexArray : UnsafeMutablePointer<DSPComplex> = bufferAsComplexArray
-                    vDSP_ctoz(bufferAsComplexArray, 2, &output, 1, vDSP_Length(frameCount / 2))
-                }
-                
-                // Perform FFT
-                vDSP_fft_zrip(fftSetup, &output, 1, frameCountInLog2, FFTDirection(FFT_FORWARD))
-                
-                var spectrum = [Float]()
-                for i in 1..<Int(frameCount / 2) {
-                    spectrum.append(sqrt((pow(output.imagp[i], 2) + pow(output.imagp[i], 2))))
-                }
-                
-                delegate?.didFourierTransform(self, output: spectrum)
-                
-                vDSP_destroy_fftsetup(fftSetup)
-            } else {
-                print("Not enough memory to setup fft")
+        let frameCount = buffer.frameLength
+        let frameCountInLog2 = vDSP_Length(log2(Double(frameCount)))
+        if let fftSetup = vDSP_create_fftsetup(frameCountInLog2, FFTRadix(FFT_RADIX2)) {
+            var realp = [Float](repeating: 0.0, count: Int(frameCount / 2))
+            var imagp = [Float](repeating: 0.0, count: Int(frameCount / 2))
+            var output = DSPSplitComplex(realp: &realp, imagp: &imagp)
+            
+            // Apply Window Function
+            var window = [Float](repeating: 0, count: Int(frameCount))
+            vDSP_hann_window(&window, vDSP_Length(frameCount), Int32(vDSP_HANN_NORM))
+            vDSP_vmul((buffer.floatChannelData?.pointee)!, 1, window, 1, (buffer.floatChannelData?.pointee)!, 1, vDSP_Length(frameCount))
+            
+            buffer.floatChannelData?.pointee.withMemoryRebound(to: DSPComplex.self, capacity: Int(frameCount)) { bufferAsComplexArray in
+                let bufferAsComplexArray : UnsafeMutablePointer<DSPComplex> = bufferAsComplexArray
+                vDSP_ctoz(bufferAsComplexArray, 2, &output, 1, vDSP_Length(frameCount / 2))
             }
+            
+            // Perform FFT
+            vDSP_fft_zrip(fftSetup, &output, 1, frameCountInLog2, FFTDirection(FFT_FORWARD))
+            
+            var spectrum = [Float]()
+            for i in 1..<Int(frameCount / 2) {
+                spectrum.append(sqrt((pow(output.imagp[i], 2) + pow(output.imagp[i], 2))))
+            }
+            
+            delegate?.didFourierTransform(self, output: spectrum)
+            
+            vDSP_destroy_fftsetup(fftSetup)
+        } else {
+            print("Not enough memory to setup fft")
+        }
     }
 }
