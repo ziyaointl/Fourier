39cfdf385f4dddc54c455c8f6151530952a7c606
Ziyao Zhang <ziyaointl@gmail.com>
2018-03-29 20:39:15 +0800

    Implement WaveformTableViewController

---
+++ Fourier/WaveformTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  WaveformTableViewController.swift
+//  Fourier
+//
+//  Created by Blocry Glass on 3/29/18.
+//  Copyright © 2018 Blocry Glass. All rights reserved.
+//
+
+import UIKit
+
+public class WaveformTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    private var waveFormViewControllers = [WaveformViewController]()
+    private var tableView: UITableView!
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return waveFormViewControllers.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let waveFormView = waveFormViewControllers[indexPath.row].view!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "plain") ?? UITableViewCell(style: .default, reuseIdentifier: "plain")
+        cell.contentView.fillSelfWith(subView: waveFormView)
+        return cell
+    }
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        tableView = UITableView()
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.fillSelfWith(subView: tableView)
+        
+        addWaveForm(withFrequency: 440)
+    }
+    
+    public func addWaveForm(withFrequency frequency: Int) {
+        let waveFormViewController = WaveformViewController()
+        waveFormViewControllers.append(waveFormViewController)
+    }
+}
+
