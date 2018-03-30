9d22b389004f47e7e70ceb0691df1abd8ae3ff49
Ziyao Zhang <ziyaointl@gmail.com>
2018-03-30 08:31:24 +0800

    Add global constants file

---
+++ Fourier/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Fourier
+//
+//  Created by Blocry Glass on 3/29/18.
+//  Copyright © 2018 Blocry Glass. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public struct Constants {
+    public struct Table {
+        static let RowHeight: CGFloat = 126.0
+    }
+    
+    public struct Colors {
+        static let Background = #colorLiteral(red: 0.9599999785, green: 0.9599999785, blue: 0.9599999785, alpha: 1)
+    }
+}
