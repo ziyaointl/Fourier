498a7a85df78cbc1a9630813a31dbcd0d7e808cb
Ziyao Zhang <ziyaointl@gmail.com>
2018-03-30 21:18:21 +0800

    Optimize AudioManager
    audioEngine is now a static instance

--- Fourier/AudioManager.swift
+++ Fourier/AudioManager.swift
@@ -11,25 +11,31 @@ import AVFoundation
 import Accelerate
 
 public class AudioManager {
-    private let audioEngine = AVAudioEngine()
+    private static let audioEngine = AVAudioEngine()
     private let fftHelper = FFTHelper()
-    private var pureTonePlayerNodes = [Int: AVPureTonePlayerNode]()
+    private let pureTonePlayerNode = AVPureTonePlayerNode()
+    private let audioFilePlayerNode = AVAudioPlayerNode()
     public weak var delegate: AudioManagerDelegate?
     public var installTap = false
+    
+    public init() {
+        AudioManager.audioEngine.attach(pureTonePlayerNode)
+        AudioManager.audioEngine.connect(pureTonePlayerNode, to: AudioManager.audioEngine.mainMixerNode, format: pureTonePlayerNode.format)
+        AudioManager.audioEngine.attach(audioFilePlayerNode)
+        AudioManager.audioEngine.connect(audioFilePlayerNode, to: AudioManager.audioEngine.mainMixerNode, format: audioFilePlayerNode.outputFormat(forBus: 0))
+    }
     
     public func play(fileWithURL url: URL) {
         if let inputFile = try? AVAudioFile(forReading: url) {
             let buffer = AVAudioPCMBuffer(pcmFormat: inputFile.processingFormat, frameCapacity: AVAudioFrameCount(inputFile.length))!
             try? inputFile.read(into: buffer)
-            let audioNode = AVAudioPlayerNode()
-            audioEngine.attach(audioNode)
-            audioEngine.connect(audioNode, to: audioEngine.mainMixerNode, format: buffer.format)
-            audioNode.scheduleBuffer(buffer, at: nil, options: .loops, completionHandler: nil)
+            audioFilePlayerNode.scheduleBuffer(buffer, at: nil, options: .loops, completionHandler: nil)
             
             // Install tap
+            // If true, a delegate is required to accept the FFT result
             if installTap {
                 let bufferSize: UInt32 = 4000
-                let mixerNode = audioEngine.mainMixerNode
+                let mixerNode = AudioManager.audioEngine.mainMixerNode
                 mixerNode.installTap(onBus: 0, bufferSize: bufferSize, format: mixerNode.outputFormat(forBus: 0)) { (buffer, time) in
                     buffer.frameLength = bufferSize
                     self.fftHelper.fourierTransform(buffer: buffer, audioManager: self) // possible memory cycle
@@ -37,28 +43,18 @@ public class AudioManager {
             }
             
             // Start Playing
-            try? audioEngine.start()
-            audioNode.play()
+            try? AudioManager.audioEngine.start()
+            audioFilePlayerNode.play()
         }
     }
     
     public func play(pureToneWithFrequency frequency: Int) {
-        // Create audio node if it does not exist
-        if pureTonePlayerNodes[frequency] == nil {
-            let node = AVPureTonePlayerNode()
-            node.frequency = Double(frequency)
-            audioEngine.attach(node)
-            audioEngine.connect(node, to: audioEngine.mainMixerNode, format: node.format)
-            pureTonePlayerNodes[frequency] = node
-        }
-        try? audioEngine.start()
-        pureTonePlayerNodes[frequency]?.play()
+        pureTonePlayerNode.frequency = Double(frequency)
+        try? AudioManager.audioEngine.start()
+        pureTonePlayerNode.play()
     }
     
     public func pause(pureToneWithFrequency frequency: Int) {
-        let node = pureTonePlayerNodes[frequency]
-        if node != nil {
-            node?.pause()
-        }
+        pureTonePlayerNode.pause()
     }
 }
